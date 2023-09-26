b34ebed0564921cb88daa4f70d3177cc1c2c07f2
kusyandi <kusyandi25@gmail.com>
2023-09-26 16:15:23 +0700

    style

--- app/Views/layouts/admin_sidebar.php
+++ app/Views/layouts/admin_sidebar.php
@@ -10,7 +10,7 @@
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+    <li class="nav-item">
         <a class="nav-link" href="<?=base_url('admin/dashboard') ?>">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
