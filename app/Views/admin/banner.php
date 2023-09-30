e6a6affd4c2722722331fa6713472d96506b529c
Cahyo <cahyo@lyrid.co.id>
2023-09-30 09:35:06 +0700

    update banner

--- app/Views/admin/banner.php
+++ app/Views/admin/banner.php
@@ -66,6 +66,19 @@
             { "width": "5%", "targets": [3,4,5] },
          ],    
       });
+
+      $('[data-toggle=confirm]').confirmation({
+         rootSelector: '[data-toggle=confirm]',
+         buttons: [
+            {
+               class: 'btn btn-sm btn-danger',
+               iconClass: 'material-icons mr-1',
+               iconContent: 'directions_railway',
+               label: 'Railway',
+               value: 'Railway',
+            }
+         ]
+      });
    });
 </script>
 <?=$this->endSection(); ?>
