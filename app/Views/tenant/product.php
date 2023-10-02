86aa6555c4619b784afdacd61dc49d0d697ae6db
kusyandi <kusyandi25@gmail.com>
2023-10-02 16:42:12 +0700

    style

--- app/Views/tenant/product.php
+++ app/Views/tenant/product.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
    <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0">Data Produk</h1>
-      <a href="<?=base_url('tenant/create-product') ?>" class="btn btn-success"><i class="fas fa-plus"></i> Tambah</a>
+      <a href="<?=base_url('tenant/create-product') ?>" class="btn btn-primary"><i class="fas fa-plus"></i> &nbsp; Tambah</a>
    </div>
 
    <div class="card">
